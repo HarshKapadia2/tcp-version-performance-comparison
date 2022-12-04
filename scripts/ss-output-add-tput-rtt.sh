65d06ee43177d8fcaf1626e2dbfdd34c20653c56
Harsh Kapadia <harshgkapadia@gmail.com>
2022-12-04 15:46:22 +0000

    Convert TPUT to standard unit

--- scripts/ss-output-add-tput-rtt.sh
+++ scripts/ss-output-add-tput-rtt.sh
@@ -6,7 +6,22 @@ touch sender-ss.txt
 
 rm -f sender-ss.txt 
 
-cleanup ()
+# convert to Kilo (K)
+# Eg: 21.2M = 21200
+# Eg: 21.2K = 21.2
+convertToKilo()
+{
+  sed '
+      s/\([0-9][0-9]*\(\.[0-9]\+\)\?\)K/\1*1/g;
+      s/\([0-9][0-9]*\(\.[0-9]\+\)\?\)M/\1*1000/g;
+      s/\([0-9][0-9]*\(\.[0-9]\+\)\?\)G/\1*1000000/g;
+      s/\([0-9][0-9]*\(\.[0-9]\+\)\?\)T/\1*1000000000/g;
+      s/\([0-9][0-9]*\(\.[0-9]\+\)\?\)P/\1*1000000000000/g;
+      s/\([0-9][0-9]*\(\.[0-9]\+\)\?\)E/\1*1000000000000000/g
+  ' </dev/stdin | bc
+}
+
+cleanup()
 {
 	# get timestamp
 	ts=$(cat sender-ss.txt | sed -e ':a; /<->$/ { N; s/<->\n//; ba; }' | grep "ESTAB" | grep "unacked" | awk '{print $1}')
@@ -15,7 +30,8 @@ cleanup ()
 	sender=$(cat sender-ss.txt | sed -e ':a; /<->$/ { N; s/<->\n//; ba; }' | grep "ESTAB" | grep "unacked" | awk '{print $6}')
 	
     # get delivery rate (throughput)
-	tput=$(cat sender-ss.txt | sed -e ':a; /<->$/ { N; s/<->\n//; ba; }' | grep "ESTAB" | grep "unacked" | grep -oP '\bpacing_rate.*\bbusy:' | awk -F '[KMbps ]' '{print $9}' | tr -d ' ')
+	tput=$(cat sender-ss.txt | sed -e ':a; /<->$/ { N; s/<->\n//; ba; }' | grep "ESTAB" | grep "unacked" | grep -oP '\bpacing_rate.*\bbusy:' | awk -F '[bps ]' '{print $8}' | tr -d ' ')
+	tput=$(echo $tput | convertToKilo)
 
 	# get rtt
 	rtt=$(cat sender-ss.txt | sed -e ':a; /<->$/ { N; s/<->\n//; ba; }' | grep "ESTAB" | grep "unacked" | grep -oP '\brto:.*\bmss' | awk -F '[:/ ]' '{print $4}' | tr -d ' ')
