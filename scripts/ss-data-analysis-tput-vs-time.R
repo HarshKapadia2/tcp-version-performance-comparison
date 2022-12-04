b7d1dc4858fa0407f953c1624e70e78df95eef17
Harsh Kapadia <harshgkapadia@gmail.com>
2022-12-04 15:46:56 +0000

    Add R scripts for RTT & TPUT

---
+++ scripts/ss-data-analysis-tput-vs-time.R
@@ -0,0 +1,22 @@
+library(ggplot2)
+
+dat <- read.csv("sender-ss.csv", header=FALSE)
+names(dat) <- c("timestamp", "sender", "tput", "rtt", "retr", "retr.total", "cwnd", "ssthresh")
+
+senders <- data.frame(table(dat$sender))
+sndlist <- senders[senders$Freq > 100,]$Var1
+
+dat <- dat[dat$sender %in% sndlist,]
+dat$sender <- factor(dat$sender)
+
+lost <- na.omit(dat[dat$retr > 0,])
+
+timestamp_min <- min(dat$timestamp)
+
+q <- ggplot(dat) + geom_line(aes(x=timestamp-timestamp_min, y=tput, colour=as.factor(sender)))
+q <- q + theme_bw() + facet_wrap(~sender, ncol=1, drop=T) + geom_vline(data=lost, aes(xintercept=timestamp-timestamp_min, colour=as.factor(sender)), alpha=0.1)
+q <- q + scale_y_continuous("Throughput (kbps)") + scale_colour_discrete("Sender") + scale_x_continuous("Time (s)") + theme(legend.position="bottom")
+
+svg("sender-ss-tput-vs-time.svg")
+print(q)
+dev.off()
