9956740b6fbe626c5c3ecf3c564f31f2af1c8794
Harsh Kapadia <harshgkapadia@gmail.com>
2022-12-04 04:52:53 +0000

    Add R & Shell scripts

---
+++ scripts/ss-output-add-tput-rtt.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+DST=$1
+
+touch sender-ss.txt
+
+rm -f sender-ss.txt 
+
+cleanup ()
+{
+	# get timestamp
+	ts=$(cat sender-ss.txt | sed -e ':a; /<->$/ { N; s/<->\n//; ba; }' | grep "ESTAB" | grep "unacked" | awk '{print $1}')
+
+	# get sender
+	sender=$(cat sender-ss.txt | sed -e ':a; /<->$/ { N; s/<->\n//; ba; }' | grep "ESTAB" | grep "unacked" | awk '{print $6}')
+	
+    # get delivery rate (throughput)
+	tput=$(cat sender-ss.txt | sed -e ':a; /<->$/ { N; s/<->\n//; ba; }' | grep "ESTAB" | grep "unacked" | grep -oP '\bpacing_rate.*\bbusy:' | awk -F '[KMbps ]' '{print $9}' | tr -d ' ')
+
+	# get rtt
+	rtt=$(cat sender-ss.txt | sed -e ':a; /<->$/ { N; s/<->\n//; ba; }' | grep "ESTAB" | grep "unacked" | grep -oP '\brto:.*\bmss' | awk -F '[:/ ]' '{print $4}' | tr -d ' ')
+
+	# get retransmissions - current, total
+	retr=$(cat sender-ss.txt | sed -e ':a; /<->$/ { N; s/<->\n//; ba; }' | grep "ESTAB" | grep "unacked" | grep -oP '\bunacked:.*\brcv_space' | awk -F '[:/ ]' '{print $4","$5}' | tr -d ' ')
+
+	# get cwnd, ssthresh
+	cwnd_ssthresh=$(cat sender-ss.txt | sed -e ':a; /<->$/ { N; s/<->\n//; ba; }' | grep "ESTAB" | grep "unacked" | grep -oP '\bcwnd:.*(\s|$)\bbytes_acked' | awk -F '[: ]' '{print $2","$4}')
+
+	# concatenate into one CSV
+	paste -d ',' <(printf %s "$ts") <(printf %s "$sender") <(printf %s "$tput") <(printf %s "$rtt") <(printf %s "$retr") <(printf %s "$cwnd_ssthresh") > sender-ss.csv
+
+	exit 0
+}
+
+trap cleanup SIGINT SIGTERM
+
+while [ 1 ]; do 
+	ss --no-header -ein dst $DST | ts '%.s' | tee -a sender-ss.txt 
+done
