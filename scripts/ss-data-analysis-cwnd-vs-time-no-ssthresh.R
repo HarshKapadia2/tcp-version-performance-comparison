127aaab1b2cafa073184294a8bc8479de63ac1ca
Harsh Kapadia <harshgkapadia@gmail.com>
2022-12-04 20:36:48 +0000

    Handle no `SSTHRESH` for `CWND` scripts

--- scripts/ss-data-analysis-cwnd-vs-time-no-ssthresh.R
+++ scripts/ss-data-analysis-cwnd-vs-time-no-ssthresh.R
@@ -14,7 +14,7 @@ lost <- na.omit(dat[dat$retr > 0,])
 
 timestamp_min <- min(dat$timestamp)
 
-q <- ggplot(dat) + geom_line(aes(x=timestamp-timestamp_min, y=cwnd, colour=as.factor(sender))) + geom_line(aes(x=timestamp-timestamp_min, y=ssthresh, colour=as.factor(sender)), linetype='twodash')
+q <- ggplot(dat) + geom_line(aes(x=timestamp-timestamp_min, y=cwnd, colour=as.factor(sender)))
 q <- q + theme_bw() + facet_wrap(~sender, ncol=1, drop=T) + geom_vline(data=lost, aes(xintercept=timestamp-timestamp_min, colour=as.factor(sender)), alpha=0.1)
 q <- q + scale_y_continuous("CWND") + scale_colour_discrete("Sender") + scale_x_continuous("Time (s)") + theme(legend.position="bottom")
 
